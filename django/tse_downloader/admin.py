21682ccb49cd95ccaed950d2bd9cbc1a4f47b681
Mohsen Karimi <mohsen1076@gmail.com>
2025-07-02 20:33:35 +0330

    Add Option model to admin panel

--- django/tse_downloader/admin.py
+++ django/tse_downloader/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from tse_downloader.models import Stock
+from tse_downloader.models import Stock, Option
+
 
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
@@ -7,3 +8,9 @@ class StockAdmin(admin.ModelAdmin):
     list_filter = ('is_in_option_chain', 'group')  # filter sidebar
     search_fields = ('name', 'symbol')     # search box
 
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ('stock', 'strike_price', 'expiration_date', 'contract_type')  # columns in list view
+    list_filter = ('stock', 'contract_type', 'expiration_date')  # filter sidebar
+    search_fields = ('stock', 'contract_type')     # search box
